14facee66ee9d3c4bd8002f656b136c08b8c01e1
Renjithvr4 <renjithweare@ymail.com>
2017-01-07 20:27:30 +0530

    removed unwanted text

--- application/views/about.php
+++ application/views/about.php
@@ -6,17 +6,10 @@
         <div class="about-content">
         <!-- about -->
                    <h3 class="tittle">About <i class="glyphicon glyphicon-user"></i></h3>
-                                
-                                <p>Having hands on experience in creating innovative designs, I do offer design
-                                        solutions which harness the new possibilities of web based communication.
-                                        <label>I do specialize in all aspects of website designing,Nam libero tempore,aspects of website designing cum soluta nobis est eligendi  theme development,
-                                possimus omnis dolor repellendus.Nam libero tempore, cum soluta nobis est eligendi  voluptatum...</label></p>
+
+
                                 <div class="clearfix"></div>
-                        <p class="nam">Nam libero tempore, cum soluta nobis est eligendi
-                                optio cumque nihil impedit quo minus id quod maxime placeat facere
-                                possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-                                Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
-                                saepe eveniet ut et voluptates repudiandae sint Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna  et molestiae non recusandae.</p>
+                        
                         <div class="more">
                                 <a href="single.html">Read More</a>
                         </div>
